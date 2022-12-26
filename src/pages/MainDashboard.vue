30597273279c5fed430eb7e0d5bb2cd779afa7f8
giopantsulaia <giorgipantsulaia@redberry.ge>
2022-12-27 02:17:45 +0400

    wip: add profile search

--- src/pages/MainDashboard.vue
+++ src/pages/MainDashboard.vue
@@ -1,6 +1,27 @@
 <template>
-  <div></div>
+  <main>
+    <div class="w-96 mx-auto flex flex-col mt-48">
+      <label for="search">Search profile</label>
+      <div>
+        <input
+          type="search"
+          name="search"
+          v-model="searchKeyword"
+          class="border-2 border-blue-400 rounded-lg p-2 outline-none"
+        />
+        <button @click="searchUser" class="px-4 text-2xl">GO</button>
+      </div>
+      <p>{{ searchKeyword }}</p>
+    </div>
+  </main>
 </template>
-<script lang="ts">
-export default {};
+<script setup lang="ts">
+import { ref } from "vue";
+import axios from "../config/axios/index.js";
+const searchKeyword = ref(null);
+function searchUser() {
+  axios
+    .post("search", { keyWord: searchKeyword.value })
+    .then((res) => console.log(res));
+}
 </script>
